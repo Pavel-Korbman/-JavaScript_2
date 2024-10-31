b4556edbb5fbd924811842cc8fa64330462200d3
Pavel <pavel@b2profit.ru>
2024-10-31 17:20:09 +0300

    2 лекция

--- 2_Lect/scrypt2.js
+++ 2_Lect/scrypt2.js
@@ -12,6 +12,23 @@ console.log(liElOne.parentElement.lastChild); // #text
 console.log(liElOne.parentElement.firstElementChild); // <li class="menu__list"><a href="#" class="menu__link">Link-1</a></li>
 
 console.log(liElOne.nextElementSibling); // <li class="menu__list"><a href="#" class="menu__link">Link-2</a></li>
+console.log(liElOne.nextElementSibling.nextElementSibling); // <li class="menu__list"><a href="#" class="menu__link">Link-3</a></li>
+console.log(liElOne.nextElementSibling.nextElementSibling.previousElementSibling); // <li class="menu__list"><a href="#" class="menu__link">Link-2</a></li>
+console.log(liElOne.previousElementSibling); // null
+console.log(liElOne.parentElement.lastElementChild.previousElementSibling); // <li class="menu__list"><a href="#" class="menu__link">Link-3</a></li>
+
+// Для родительского элемента parentNode и parentElement практически всегда возвращают один и тот же элемент, за исключением случаев, когда parentNode элемента — не DOM Element. В таком случае parentElement возвращает null.
+
+document.body.parentNode // Элемент <html> 
+document.body.parentElement // Элемент <html> 
+document.documentElement.parentNode // Нода document 
+document.documentElement.parentElement // null 
+(document.documentElement.parentNode === document) // true 
+(document.documentElement.parentElement === document) // false
+
+
+
+
 
 
 
