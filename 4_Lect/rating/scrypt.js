6a29ccaf137fdaa667fca55503c4b6521d29574c
Pavel <pavel@b2profit.ru>
2024-11-13 22:50:29 +0300

    4 лекция

--- 4_Lect/rating/scrypt.js
+++ 4_Lect/rating/scrypt.js
@@ -1,2 +1,18 @@
+const form = document.querySelector('.form');
+const buttons = document.querySelectorAll('.rating_sign_input');
 
 
+
+buttons.forEach(radio => {
+    radio.addEventListener('change', function () {
+        const checkedNumber = document.querySelector('.rating_sign_input:checked').value || undefined;
+
+        buttons.forEach(item => {
+            const currentLabel = item.previousElementSibling;
+            if (item.value <= checkedNumber) {
+                currentLabel.classList.add('check');
+            } else { currentLabel.classList.remove('check'); }
+        });
+
+    });
+});
