ad0f1fb0923b4e4df2a53205948d8116342bd113
Pavel <pavel@b2profit.ru>
2024-11-14 11:40:57 +0300

    4 лекция

--- 4_Lect/rating/scrypt.js
+++ 4_Lect/rating/scrypt.js
@@ -1,18 +1,17 @@
 const form = document.querySelector('.form');
-const buttons = document.querySelectorAll('.rating_sign_input');
-
-
+const buttons = form.querySelectorAll('.rating_sign_input');
+const ratingVal = form.querySelector('.rating_value');
 
 buttons.forEach(radio => {
     radio.addEventListener('change', function () {
         const checkedNumber = document.querySelector('.rating_sign_input:checked').value || undefined;
-
+        //console.log(`Рейтинг: ${checkedNumber}`);
+        ratingVal.textContent = checkedNumber;
         buttons.forEach(item => {
             const currentLabel = item.previousElementSibling;
             if (item.value <= checkedNumber) {
                 currentLabel.classList.add('check');
             } else { currentLabel.classList.remove('check'); }
         });
-
     });
 });
