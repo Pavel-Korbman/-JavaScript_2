6362637c30a4771f6ab5013a179afddfe16c5bb0
Pavel <pavel@b2profit.ru>
2024-11-19 20:34:17 +0300

    5 сем

--- 5_Sem/data.js
+++ 5_Sem/data.js
@@ -5,16 +5,16 @@ const dataInfo = `
 "url": "https://cdn2.thecatapi.com/images/ef6.jpg",
 "wight": 500,
 "height": 334
-}
+},
 {
 "id": "bpj",
 "url": "https://cdn2.thecatapi.com/images/bpj.jpg",
 "wight": 796,
 "height": 1061
-}
+},
 {
 "id": "dt7",
-"url": "https://cdn2.thecatapi.com/images/dt7.jpg",
+"url": "https://cdn2.thecatapi.com/images/dt7.png",
 "wight": 765,
 "height": 1024
 }
--- 5_Sem/scrypt.js
+++ 5_Sem/scrypt.js
@@ -1,1 +1,14 @@
 // Задание 1 
+const data = JSON.parse(dataInfo);
+console.log(data);
+const divEl = document.querySelector('.content');
+data.forEach(e => {
+    const imgEl = document.createElement('img');
+    imgEl.src = e.url;
+    imgEl.style = `width: ${e.width}px`;
+    imgEl.style = `height: ${e.height}px`;
+    divEl.appendChild(imgEl);
+
+
+    //console.log(e.url);
+});
