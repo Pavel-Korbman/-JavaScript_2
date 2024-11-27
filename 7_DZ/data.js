23e8ab2e83897f0c64a0d832dfb7ffec47ee2595
Pavel <pavel@b2profit.ru>
2024-11-27 05:56:43 +0300

    7 ДЗ

--- 7_DZ/data.js
+++ 7_DZ/data.js
@@ -5,35 +5,51 @@ const dataInfo = `
 "img": "img/banner_1.jpg",
 "price": "52.00",
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
+
 },
 {
 "name": "ELLERY X M'O CAPSULE",
 "img": "img/banner_2.jpg",
 "price": "52.00",
+"color": "Black",
+"size": "L",
+"quantity": 1,
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
 },
 {
 "name": "ELLERY X M'O CAPSULE",
 "img": "img/banner_3.jpg",
 "price": "52.00",
+"color": "Red",
+"size": "XL",
+"quantity": 1,
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
 },
 {
 "name": "ELLERY X M'O CAPSULE",
 "img": "img/banner_4.jpg",
 "price": "52.00",
+"color": "Black",
+"size": "L",
+"quantity": 1,
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
 },
 {
 "name": "ELLERY X M'O CAPSULE",
 "img": "img/banner_5.jpg",
 "price": "52.00",
+"color": "Black",
+"size": "L",
+"quantity": 1,
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
 },
 {
 "name": "ELLERY X M'O CAPSULE",
 "img": "img/banner_6.jpg",
 "price": "52.00",
+"color": "Black",
+"size": "L",
+"quantity": 1,
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi."
 }
 ]
--- 7_DZ/script.js
+++ 7_DZ/script.js
@@ -1,14 +1,16 @@
+"use strict";
 const data = JSON.parse(dataInfo);
 const goodsEl = document.querySelector('.goods');
-
+let count = 0;
 data.forEach(e => {
     const item = document.createElement('div');
     goodsEl.appendChild(item);
     item.classList.add('item');
 
+
     const aEl = document.createElement('a');
     item.appendChild(aEl);
-    aEl.href = 'card/card.html';
+    aEl.href = '#';
 
     const itemPhoto = document.createElement('div');
     aEl.appendChild(itemPhoto);
@@ -18,12 +20,13 @@ data.forEach(e => {
     imgEl.src = e.img;
     imgEl.alt = 'photo';
     itemPhoto.appendChild(imgEl);
-    
+
     const hooverEl = document.createElement('img');
     hooverEl.src = 'img/hover.svg';
     hooverEl.alt = 'card';
     itemPhoto.appendChild(hooverEl);
     hooverEl.classList.add('hoover__on');
+    hooverEl.id = count;
 
     const itemName = document.createElement('h3');
     aEl.appendChild(itemName);
@@ -37,61 +40,88 @@ data.forEach(e => {
     aEl.appendChild(itemPrice);
     itemPrice.textContent = `$${e.price}`;
 
-
-
-
-
-
-    // const imgEl = document.createElement('img');
-    // imgEl.src = e.img;
-    // imgEl.alt = 'photo';
-    // liEl.appendChild(imgEl);
-    // imgEl.classList.add('img');
-
-    // const contentEl = document.createElement('div');
-    // liEl.appendChild(contentEl);
-    // contentEl.classList.add('content');
-
-    // const nameEl = document.createElement('h2');
-    // contentEl.appendChild(nameEl);
-    // nameEl.textContent = 'MANGO PEOPLE T-SHIRT';
-
-    // const priceEl = document.createElement('p');
-    // contentEl.appendChild(priceEl);
-    // priceEl.textContent = 'Price: ';
-    // const price = document.createElement('span');
-    // priceEl.appendChild(price);
-    // price.classList.add('price');
-    // price.textContent = `$${e.price}`;
-
-    // const colorEl = document.createElement('p');
-    // contentEl.appendChild(colorEl);
-    // colorEl.textContent = 'Color: ';
-    // const color = document.createElement('span');
-    // colorEl.appendChild(color);
-    // color.classList.add('color');
-    // color.textContent = `${e.color}`;
-
-    // const sizeEl = document.createElement('p');
-    // contentEl.appendChild(sizeEl);
-    // sizeEl.textContent = 'Size: ';
-    // const size = document.createElement('span');
-    // sizeEl.appendChild(size);
-    // size.classList.add('color');
-    // size.textContent = `${e.size}`;
-
-    // const quantityEl = document.createElement('p');
-    // contentEl.appendChild(quantityEl);
-    // quantityEl.textContent = 'Quantity:';
-    // const quantity = document.createElement('span');
-    // quantityEl.appendChild(quantity);
-    // quantity.classList.add('quantity');
-    // quantity.textContent = `${e.quantity}`;
-
-    // const closeIcon = document.createElement('img');
-    // closeIcon.src = '../img/close.svg';
-    // closeIcon.alt = 'close';
-    // liEl.appendChild(closeIcon);
-    // closeIcon.classList.add('close');
-    
+    count++;
 });
+
+const dataCard = [];
+const ulEl = document.querySelector('.list');
+goodsEl.addEventListener('click', function (element) {
+    dataCard.push(data[element.target.id]);
+    ulEl.innerHTML = "";
+    let count = 0;
+    dataCard.forEach(e => {
+        const liEl = document.createElement('li');
+        ulEl.appendChild(liEl);
+        liEl.classList.add('product_card');
+        liEl.id = count;
+
+        const imgEl = document.createElement('img');
+        imgEl.src = e.img;
+        imgEl.alt = 'photo';
+        liEl.appendChild(imgEl);
+        imgEl.classList.add('img_card');
+
+        const contentEl = document.createElement('div');
+        liEl.appendChild(contentEl);
+        contentEl.classList.add('content_card');
+
+        const nameEl = document.createElement('h2');
+        contentEl.appendChild(nameEl);
+        nameEl.textContent = 'MANGO PEOPLE T-SHIRT';
+
+        const priceEl = document.createElement('p');
+        contentEl.appendChild(priceEl);
+        priceEl.textContent = 'Price: ';
+        const price = document.createElement('span');
+        priceEl.appendChild(price);
+        price.classList.add('price');
+        price.textContent = `$${e.price}`;
+
+        const colorEl = document.createElement('p');
+        contentEl.appendChild(colorEl);
+        colorEl.textContent = 'Color: ';
+        const color = document.createElement('span');
+        colorEl.appendChild(color);
+        color.classList.add('color');
+        color.textContent = `${e.color}`;
+
+        const sizeEl = document.createElement('p');
+        contentEl.appendChild(sizeEl);
+        sizeEl.textContent = 'Size: ';
+        const size = document.createElement('span');
+        sizeEl.appendChild(size);
+        size.classList.add('color');
+        size.textContent = `${e.size}`;
+
+        const quantityEl = document.createElement('p');
+        contentEl.appendChild(quantityEl);
+        quantityEl.textContent = 'Quantity:';
+        const quantity = document.createElement('span');
+        quantityEl.appendChild(quantity);
+        quantity.classList.add('quantity');
+        quantity.textContent = `${e.quantity}`;
+
+        const closeEl = document.createElement('a');
+        liEl.appendChild(closeEl);
+        closeEl.classList.add('close');
+
+        const closeIcon = document.createElement('img');
+        closeIcon.src = 'img/close.svg';
+        closeIcon.alt = 'close';
+        closeEl.appendChild(closeIcon);
+        closeIcon.classList.add('close_img');
+
+        count++;
+
+        closeEl.addEventListener('click', function () {
+            dataCard.splice(closeEl.parentNode.id, 1);
+            closeEl.parentNode.remove();
+        });
+
+    });
+
+});
+
+
+
+
